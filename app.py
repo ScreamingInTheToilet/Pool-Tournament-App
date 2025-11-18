9bcf6659e6e679bce4886ad27b407e12cddfda00
ScreamingInTheToilet <121836498+ScreamingInTheToilet@users.noreply.github.com>
2025-11-18 11:24:33 +1030

    Skelstructure (#2)
    * I want to commit
    
    * Create skeleton structure for the pool tournament
    
    * Update app.py
    
    change tournament_list into player_list
    
    Co-authored-by: martin-monis-chamonix-it <153473453+martin-monis-chamonix-it@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: martin-monis-chamonix-it <153473453+martin-monis-chamonix-it@users.noreply.github.com>

---
+++ app.py
@@ -0,0 +1,51 @@
+import json
+
+
+
+player_list = []
+
+'''
+
+functions:
+
+randomizer - randomly assigning numbers to the player, 
+before inserting them into list indexes corresponding to the player's assigned number
+
+pairing - pairs players next to each other in a list order.
+
+view - view player's status and background info by providing their ID
+
+eliminate - give the user the choice to eliminate players.
+
+session_error - If an error occurs, this function handles the error.
+
+insert_playerdata - If the User wishes to insert a premade excel sheet, 
+they can do so through this function
+
+rounds - proceed to another round.
+'''
+
+
+def randomizer(list):
+    pass
+
+
+def pairing(list):
+    '''
+    creating match ups with players next to each other.
+    '''
+    pass
+
+def view(player_ID):
+    pass
+
+def eliminate(player_ID=None, player_nun=None):
+    '''
+    In each match, the user gets to choose what player they want to eliminate.
+    '''
+    pass
+
+def rounds():
+    pass
+
+
